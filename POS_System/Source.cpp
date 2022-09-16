e914bb8f483d926b768c9db86ba4b197a5ebdc66
jigs074 <90673637+jigs074@users.noreply.github.com>
2022-09-16 01:18:43 -0400

    Created a product function which will make a text file for the records of product , The problem which has occured is that it only displays the record which is most latest.

--- POS_System/Source.cpp
+++ POS_System/Source.cpp
@@ -2,10 +2,40 @@
 
 #include<iostream>
 #include<fstream>
+#define DISCOUNT 10 
 
 using namespace std;
+class POS_System {
+	
 
+	string product; 
+	float price; 
+public : 
+	void create_Product() {
+
+		cout << "Product name:"<<endl; 
+		cin >> product; 
+
+		cout << product << "'s price:" << endl; 
+		cin >> price; 
+
+		ofstream Productlist;
+		
+		Productlist.open("Productrecords.txt"); 
+		Productlist << "Product name: " << product << endl; 
+		Productlist << product << "'s price: " << price << endl;
+
+		Productlist.close();
+
+	}
+
+};
 int main()
 {
-	cout << "Hello World" << endl;
+	
+	POS_System p;
+	p.create_Product();
+	p.create_Product(); 
+
+
 }
